cd350a05abc3930e72a6415003c7823d544a7da5
Don Gumayagay <dngumayagay@gmail.com>
2023-07-28 23:37:59 +0800

    switch with classify

--- src/routes/api/classify/+server.ts
+++ src/routes/api/classify/+server.ts
@@ -1,10 +1,11 @@
-import { sentiment_classifier } from '$lib/transformer.server';
+// import { sentiment_classifier } from '$lib/transformer.server';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url }) => {
     const text = url.searchParams.get('text')
     if (!text) throw error(400, 'provide "text" query params')
-    const result = await sentiment_classifier(text)
-    return json({ text, ...result[0] })
+    // const result = await sentiment_classifier(text)
+    // return json({ text, ...result[0] })
+    return new Response('classify')
 };
