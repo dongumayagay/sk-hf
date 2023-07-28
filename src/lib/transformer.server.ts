f905bebb963a632180064751ea883825e767175d
Don Gumayagay <dngumayagay@gmail.com>
2023-07-28 23:10:41 +0800

    summarize

--- src/lib/transformer.server.ts
+++ src/lib/transformer.server.ts
@@ -1,3 +1,4 @@
 import { pipeline } from '@xenova/transformers';
 
-export const sentiment_classifier = await pipeline('sentiment-analysis');
+export const sentiment_classifier = await pipeline('sentiment-analysis', 'Xenova/distilbert-base-uncased-finetuned-sst-2-english');
+export const summarizer = await pipeline('summarization', 'Xenova/distilbart-cnn-6-6');
---
+++ src/routes/api/summarize/+server.ts
@@ -0,0 +1,10 @@
+import { summarizer } from '$lib/transformer.server';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ url }) => {
+    const text = url.searchParams.get('text')
+    if (!text) throw error(400, 'provide "text" query params')
+    const result = await summarizer(text)
+    return json({ text, ...result[0] })
+};
