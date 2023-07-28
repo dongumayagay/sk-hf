ab92672c04926f0c90bf790eca56888ef5762aa7
Don Gumayagay <dngumayagay@gmail.com>
2023-07-28 23:32:50 +0800

    remove summarize for test

--- src/routes/api/summarize/+server.ts
+++ src/routes/api/summarize/+server.ts
@@ -1,10 +1,11 @@
-import { summarizer } from '$lib/transformer.server';
+// import { summarizer } from '$lib/transformer.server';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url }) => {
     const text = url.searchParams.get('text')
     if (!text) throw error(400, 'provide "text" query params')
-    const result = await summarizer(text)
-    return json({ text, ...result[0] })
+    // const result = await summarizer(text)
+    // return json({ text, ...result[0] })
+    return new Response('summarize')
 };
