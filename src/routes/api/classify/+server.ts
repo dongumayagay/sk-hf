0bd9ec6c2accf5a715994a477ac87a1a2a874c67
Don Gumayagay <dngumayagay@gmail.com>
2023-08-23 22:04:23 +0800

    fix classify

--- src/routes/api/classify/+server.ts
+++ src/routes/api/classify/+server.ts
@@ -2,10 +2,13 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
+import { pipeline } from '@xenova/transformers';
+
+const sentiment_classifier = await pipeline('sentiment-analysis', 'Xenova/distilbert-base-uncased-finetuned-sst-2-english');
+
 export const GET: RequestHandler = async ({ url }) => {
     const text = url.searchParams.get('text')
     if (!text) throw error(400, 'provide "text" query params')
-    // const result = await sentiment_classifier(text)
-    // return json({ text, ...result[0] })
-    return new Response('classify')
+    const result = await sentiment_classifier(text)
+    return json({ text, ...result[0] })
 };
